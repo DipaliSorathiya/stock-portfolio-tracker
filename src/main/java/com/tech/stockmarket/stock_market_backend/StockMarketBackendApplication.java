dbcba21a07444152f9b46e19242d85081c968247
dipali sorthiya <dsorathiya77@gmail.com>
2025-06-08 18:05:50 +0530

    🔧 Added Redis caching for stock price fetching

--- src/main/java/com/tech/stockmarket/stock_market_backend/StockMarketBackendApplication.java
+++ src/main/java/com/tech/stockmarket/stock_market_backend/StockMarketBackendApplication.java
@@ -2,8 +2,10 @@ package com.tech.stockmarket.stock_market_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = "com.tech.stockmarket.stock_market_backend")
+@EnableCaching
 public class StockMarketBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/tech/stockmarket/stock_market_backend/services/StockService.java
+++ src/main/java/com/tech/stockmarket/stock_market_backend/services/StockService.java
@@ -1,9 +1,11 @@
 package com.tech.stockmarket.stock_market_backend.services;
 
+
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cache.annotation.Cacheable;
 
 
 @Service
@@ -12,7 +14,11 @@ public class StockService {
     private final String API_KEY = "DTZYCJ2L011JGU80";
     private final String BASE_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={apikey}";
 
+
+    @Cacheable(value = "stockPrices", key = "#symbol")
     public double getStockPrice(String symbol) {
+        System.out.println("📡 Fetching stock price for " + symbol + " from external API (not Redis)");
+
         RestTemplate restTemplate = new RestTemplate();
         String url = BASE_URL.replace("{symbol}", symbol).replace("{apikey}", API_KEY);
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
